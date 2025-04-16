48827fdbff4b5931e87bb0b87815d52f55c059a7
Austin Fletcher <49542016+astn7278@users.noreply.github.com>
2025-04-16 00:09:39 -0700

    Add space check errors

--- script.js
+++ script.js
@@ -14,32 +14,39 @@ const createBoard = (function() {
     
     //Creates and returns copy of board
     const getBoard = () => board.map(row => [...row]);
-    //Starts new game
-    const newGame = function() {
-        let gameOver = false;
-        let currentPlayer = "X";
-        let moves = 0; 
-    }
     
-    //checks for blank space within range,
+    let gameOver = false;
+    let currentPlayer = "X";
+    let moves = 0; 
+    
+    
+    //checks for blank space within range
     const addMarker = (column, row) => {
         const openSpace = (col, rw) => {
             if (rw < 0 || rw >= board.length || col < 0 || col >= board[0].length) {
-                console.log("Marker not added!")
+                console.log("Invalid spot");
                 return false;
-            }            
+            }
+            
+            if (board[row][column] !== ''){
+                console.log("Spot filled already");
+                return false;
+            }
+
             return board[rw][col] === '';
         }
-
+        //places marker and switches players
         if (openSpace(column, row)){
-            board[row][column] = "X";
-            console.log("Marker added!")
-            console.log(createBoard.getBoard())
-        }
+            board[row][column] = currentPlayer;
+            console.log("Marker added!");
+            console.log(createBoard.getBoard());
+            moves++;
+            currentPlayer = currentPlayer === "X" ? "O" : "X";
+        } 
     }
 
     return {
-        getBoard, newGame, addMarker
+        getBoard, addMarker
     };
 })(); //IIFE
 
