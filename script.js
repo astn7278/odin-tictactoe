8b4893b5b8b8488da77d29b8cebc8cbbaac37a8b
Austin Fletcher <49542016+astn7278@users.noreply.github.com>
2025-04-16 21:15:10 -0700

    Create win check logic

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const createBoard = (function() {
     //Creates and returns copy of board
     const getBoard = () => board.map(row => [...row]);
     
-    let gameOver = false;
+    let running = true;
     let currentPlayer = "X";
     let moves = 0; 
     
@@ -38,22 +38,65 @@ const createBoard = (function() {
         //places marker, logs board, increments moves, switches players
         if (openSpace(column, row)){
             board[row][column] = currentPlayer;
+            moves++;
             console.log("Marker added!");
             console.log(createBoard.getBoard());
-            moves++;
-            currentPlayer = currentPlayer === "X" ? "O" : "X";
+            
+            const winCheck = checkWin();
+                if (winCheck) {
+                    winner = winCheck;
+                    running = false;
+                    console.log('Player ${`winner`} wins!')
+                } else if (moves === 9) {
+                    running = false;
+                    console.log("Draw game")
+                } else {
+                    currentPlayer = currentPlayer === "X" ? "O" : "X";
+
+                }
+            }
+
         } 
-    }
+
 
     return {
         getBoard, addMarker
     };
 })(); //IIFE
 
+//move(x,y)
+const move = createBoard.addMarker;
+
+//check for winning pattern
+const checkWin = () => {
+    const board = createBoard.getBoard();
+    const lines = [
+        // Rows
+        [ [0,0], [0,1], [0,2] ],
+        [ [1,0], [1,1], [1,2] ],
+        [ [2,0], [2,1], [2,2] ],
+        // Columns
+        [ [0,0], [1,0], [2,0] ],
+        [ [0,1], [1,1], [2,1] ],
+        [ [0,2], [1,2], [2,2] ],
+        // Diagonals
+        [ [0,0], [1,1], [2,2] ],
+        [ [0,2], [1,1], [2,0] ],
+    ];
+    //check for matches, return player letter if true otherwise false
+    for (let line of lines) {
+        const [a, b, c] = line;
+        if (
+            board[a[0]][a[1]] &&
+            board[a[0]][a[1]] === board[b[0]][b[1]] &&
+            board[a[0]][a[1]] === board[c[0]][c[1]]
+        ) {
+            return board[a[0]][a[1]];
+        }
+    }
+
+    return null;
+};
 
 
 
-
-// const printBoard = (function() {
-//     console.log(createBoard.getBoard())
-// });
